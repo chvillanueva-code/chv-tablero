62ba32d0c94ee47d34026dbd19d2a9836c28a1a7
chvillanueva-code <ch.villanueva@gmail.com>
2026-09-21 19:47:49 -0300

    Conecta el tablero a la App web de Sheets

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CHV_CONFIG = {
-  WEBAPP_URL: "",
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwEW9PpF7DM-pgs_eR7KK-xp8PxaHxwhIW4M1RQRBc2mQvz3DsS4WA-IIwGGCpzvCj48w/exec",
   TOKEN: "210078",
   SHEET_ID: "1a4_ryH0pv0SleJzlSHgFI6JrjAmMJbl8FZdaPb7epR8"
 };
