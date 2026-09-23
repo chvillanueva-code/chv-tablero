fa59174a02e6287a6503e342cd5e862044b9be82
chvillanueva-code <ch.villanueva@gmail.com>
2026-09-23 02:25:46 -0300

    Nueva URL de Apps Script v2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CHV_CONFIG = {
-  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwEW9PpF7DM-pgs_eR7KK-xp8PxaHxwhIW4M1RQRBc2mQvz3DsS4WA-IIwGGCpzvCj48w/exec",
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzb-8WoP7Ts7xJPhQSMG1lVko0i8ycQO75jUJAtwBHXgeOmqkeHDdN3RDa61g6b9EQwPA/exec",
   TOKEN: "250578",
   SHEET_ID: "1a4_ryH0pv0SleJzlSHgFI6JrjAmMJbl8FZdaPb7epR8"
 };
