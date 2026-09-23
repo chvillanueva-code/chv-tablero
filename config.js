8430eefe1575e6fcc746150b0c01107699e7c1a2
chvillanueva-code <ch.villanueva@gmail.com>
2026-09-23 03:01:24 -0300

    URL Apps Script v3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CHV_CONFIG = {
-  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzb-8WoP7Ts7xJPhQSMG1lVko0i8ycQO75jUJAtwBHXgeOmqkeHDdN3RDa61g6b9EQwPA/exec",
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyZBqP-F-KWVTEf5zpYlaJzZI0lZpSNEibzPjAFHE1nT2_jQJTDohzkD4_AvH17aCXCjA/exec",
   TOKEN: "250578",
   SHEET_ID: "1a4_ryH0pv0SleJzlSHgFI6JrjAmMJbl8FZdaPb7epR8"
 };
