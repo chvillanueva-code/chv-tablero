936aef83cd34f8169ea24138065e0de1d8ead1b9
chvillanueva-code <ch.villanueva@gmail.com>
2026-09-23 03:55:27 -0300

    Usuarios CHV y MLV

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 window.CHV_CONFIG = {
   WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyZBqP-F-KWVTEf5zpYlaJzZI0lZpSNEibzPjAFHE1nT2_jQJTDohzkD4_AvH17aCXCjA/exec",
   TOKEN: "250578",
-  SHEET_ID: "1a4_ryH0pv0SleJzlSHgFI6JrjAmMJbl8FZdaPb7epR8"
+  SHEET_ID: "1a4_ryH0pv0SleJzlSHgFI6JrjAmMJbl8FZdaPb7epR8",
+  USERS: [
+    { usuario: "CHV", clave: "250578", nombre: "CHV" },
+    { usuario: "MLV", clave: "250578", nombre: "MLV" }
+  ]
 };
