e065fa5ca02647e6a4f585b06d19f9119af593c3
chvillanueva-code <ch.villanueva@gmail.com>
2026-09-23 04:04:14 -0300

    Apps Script v4 usuarios y compartidos

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CHV_CONFIG = {
-  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbyZBqP-F-KWVTEf5zpYlaJzZI0lZpSNEibzPjAFHE1nT2_jQJTDohzkD4_AvH17aCXCjA/exec",
+  WEBAPP_URL: "https://script.google.com/macros/s/AKfycbz2ciT4vVQIEkB1uq-sapd3wn6pUXxexzlWpv9_MCdkgXsp27xIw2tPX4y4g2vQdF7CYA/exec",
   TOKEN: "250578",
   SHEET_ID: "1a4_ryH0pv0SleJzlSHgFI6JrjAmMJbl8FZdaPb7epR8",
   USERS: [
